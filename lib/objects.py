aff47a89dd35439e29bf34c169142b5c0a219bfc
Kevin Johnson <kevinstuartjohnson@gmail.com>
2015-03-17 19:12:25 -0400

    Sloof-atkin-Elkies scafold

--- lib/objects.py
+++ lib/objects.py
@@ -2,8 +2,13 @@ from sympy import *
 from utils import * 
 from random import randint 
 
-class AlgebraicSet(object):
+"""
 
+Classes for algebraic objects
+
+"""
+
+class AlgebraicSet(object):
 	"""
 	Algebraic set defined by polynomials over a field 
 	Currently only finite fields of prime order are supported.
@@ -11,7 +16,7 @@ class AlgebraicSet(object):
 
 	def __init__(self, equations,field):
 		if field.characteristic() <= 3:
-			raise NotImplementedError("Only finite fields of prime order are supported.")
+			raise NotImplementedError("Only finite fields of prime order are currently supported.")
 
 		symbols 	= set()
 		polynomials = list()
@@ -36,34 +41,37 @@ class AlgebraicSet(object):
 		return True  
 
 	def points(self):
+		""" Returns points which satisfy all defining polynomials """
 		return solve_poly_system(self.polynomials)
 
-	# def set_polynomials(self,equations):
+	def count_points(self):
+		raise NotImplementedError("Still figuring out this http://ac.els-cdn.com/S0747717101904705/1-s2.0-S0747717101904705-main.pdf?_tid=2443a374-ccfa-11e4-beb9-00000aacb35e&acdnat=1426633725_4c513d8c3a848312863104ac79f4ce58")
+		""" 
+		Number of points satisfying all equations with the methods of 
 
-	# 	polynomials = list()
-	# 	symbols 	= set()
+		http://ac.els-cdn.com/S0747717101904705/1-s2.0-S0747717101904705-main.pdf?_tid=604b8ef6-ccee-11e4-9e66-00000aab0f01&acdnat=1426628671_1e73adbeb0d2deb89d89b55aa63774d1
 
-	# 	for f in equations:
-	# 		symbols = set(symbols | f.atoms(Symbol))
-	# 		polynomials.append(poly(f,symbols,domain=self.field))
+		"""
+		pass 
 
-	# 	self.symbols = symbols
-	# 	self.polynomials = polynomials
 
 
 class Variety(AlgebraicSet):
-
 	""" An irrecucible algebraic set"""
-
 	def __init__(self, equations, field):
 		AlgebraicSet.__init__(self,equations,field)
 
+	def reducible(self):
+		raise NotImplementedError("still figuring out proper criteria over finite fields")
+		""" 
+		Returns true if algebraic set can be written as the 
+		product of two proper irreducible subsets 
+		"""
+		pass 
 
 
 class AbelianVariety(Variety):
-
 	""" A complete group variety with an operation on its points"""
-
 	def __init__(self, equations, field, operation):
 		Variety.__init__(self,equations,field)
 		self.operation = operation
@@ -101,6 +109,7 @@ class EllipticCurve(AbelianVariety):
 			raise NotImplementedError("Only elliptic curves with non-zero coeffiencts are supported.")
 
 	def get_points(self):
+		""" brute force list of points on E  """
 		p = self.field.characteristic()
 		list_of_points = list()
 
@@ -112,6 +121,7 @@ class EllipticCurve(AbelianVariety):
 		return list_of_points
 
 	def random_point(self):
+		""" Lazy point finding algorithm """
 		p = self.field.characteristic()
 		x = randint(0,p)
 		y = randint(0,p)
@@ -123,6 +133,7 @@ class EllipticCurve(AbelianVariety):
 		return (x,y)
 
 	def scalar_mult(self,num,point):
+		""" Scalar multiplication of a point on E """
 		if num == 0:
 			return (symbols('O'),symbols('O'))
 	 	if num == 1:
@@ -132,30 +143,62 @@ class EllipticCurve(AbelianVariety):
 		else:
 			return self.add(point,self.scalar_mult(num - 1,point))
 
-
 	def order(self):
-		""" Schoof's algorithm to count number of points on elliptic curve """
+		raise NotImplementedError("Still figuring out how to determine t")
+		""" 
 
-		p = self.field.characteristic()
+		Determine the number of points of E over F 
+		using the Schoof-Elkies-Atkin Algorithm. 
 
-		N = 4*sqrt(p) + 1 
+		http://en.wikipedia.org/wiki/Schoof%E2%80%93Elkies%E2%80%93Atkin_algorithm
 
-		L = easy_factor(N)
-		traces = list()
+		"""
 
-		for i in xrange(0,len(L)-1):
-			traces[i] = trace_frob(p,L[i])
+		# All variables used in algorithm 
+		m, l, A, E, q = 1, 2, list(), list(), self.field.characteristic
 
-		return p + 1 - chinese_remainder_theorem(L,traces,len(L))
+		# Find possible traces 
+		while m < 4*sqrt(q):
+			if atkin(l):
+				ll = eigen_value()
+				t = (ll + q/ll) % l 
+				E.append((t,l))
+			else:
+				for t in possible_traces(l):
+					A.append((t,l))
 
+			m *= l 
+			l = next_prime(l)
 
+		
+		# Find t using sets A,E? using CRT and BSGS???
+		# http://people.cs.nctu.edu.tw/~rjchen/ECC2009/30_SchoofElkiesAtkin.pdf
 
-		# use  Schoof-Elkies-Atkin Algorithm 
+		# return number of points on E. 
+		return q + 1 - t 
 
+		# These are really specific to order finding.
+		def atkin(l):
+			""" Determine if l is a atkin prime or not """
+			pass 
 
+		def eigen_value():
+			""" Find Igenvalue 
 
+			more info here;
+
+			http://people.cs.nctu.edu.tw/~rjchen/ECC2009/30_SchoofElkiesAtkin.pdf
+
+			"""
+			pass 
+
+		def possible_traces(l):
+			""" Find all traces of mod l """
+			pass 
+
+			
 class HyperEllipticCurve(AbelianVariety):
-	""" HyperEllipticCurves """
+	""" Class for HyperEllipticCurves """
 	def __init__(self, equations,field):
 
 		def operation((a1,b1),(a2,b2)):	
@@ -167,59 +210,35 @@ class HyperEllipticCurve(AbelianVariety):
 
 			# Composition 
 
-			d1, e1, e2 	= gcdex(a1,a2)
-			d2, c1, c2 	= gcdex(d1, b1 + b2)
+			d1, e1, c1 	= gcdex(a1,a2)
+			d, e2, c2 	= gcdex(d1, b1 + b2)
 
-			s1 = c1*e1
-			s2 = c1*e2
-			s3 = c2 
+			a3 = (a1*a2) / d**2 
 
-			u1 = (a1*a2) / d2**2 
-			v1 = reduced(s1*a1*b2 + s1*a2*b1 + s3*b1*b2 + s3*f, [u1])[1]
-
-			u1 = simplify(u1)
-			v1 = simplify(v1)
+			# b_3 = reduce(((c2*c1*a1 + c2*e1*a2 + e2*(b1*b2 + f)) / d),[a_3])[1]
+			print reduce(((c2*c1*a1 + c2*e1*a2 + e2*(b1*b2 + f)) / d),[a3])
 
 			# Reduction 
 
-			u2 = (f - v1**2) / u1 
-			v2 = reduced((-v1), [u2])[1]
+			# u2 = (f - v1**2) / u1 
+			# v2 = reduced((-v1), [u2])[1]
 
-			while degree(u2) > g:
-				u2 = (f - v2**2) / u2
-				v2 = reduced((-v2), [u2])[1]
+			# while degree(u2) > g:
+			# 	u2 = (f - v2**2) / u2
+			# 	v2 = reduced((-v2), [u2])[1]
 
-			if coeffs(u2)[0] != 1:
-				u2 = u2/ coeffs(u2)[0]
+			# if coeffs(u2)[0] != 1:
+			# 	u2 = u2/ coeffs(u2)[0]
 
-			u2 = simplify(u2)
-			v2 = simplify(v1)
+			# u2 = simplify(u2)
+			# v2 = simplify(v1)
 
-			return (u2,v2)
+			# return (a3,b3)
 
 		AbelianVariety.__init__(self,equations,field,operation)
 
 
-	def number_of_points(self):
-		pass
 
-
-x,y = symbols('x,y')
-
-
-f = y**2 - x**3 - x - 1 
-g = x**3 + x**2 + x 
-h = x**3 + x**2 + x + 1  
-
-
-F = FiniteField(43)
-
-E = EllipticCurve([f],F)
-F = HyperEllipticCurve([f],F)
-
-F.add((h,g),(h,g))
-
-print E.order()
 
 
 
